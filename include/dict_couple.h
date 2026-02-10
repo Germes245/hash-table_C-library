3ed5d522cb74399fa76255909468bbfca4ae4a69
shya <dan119@bk.ru>
2026-02-10 20:05:53 +0300

    first possible steps for integer

--- include/dict_couple.h
+++ include/dict_couple.h
@@ -1,11 +1,8 @@
 typedef struct{
-    char *key;
-    char *value;
+    int key;
+    int value;
 } dict_couple;
 
-#define create_and_copy(destination, source) destination = malloc(strlen(source)+1);\
-strcpy(destination, source)
-
-dict_couple dict_couple_init(char key[], char value[]);
-void dict_couple_change_value(dict_couple couple, char value[]);
+dict_couple dict_couple_init(int key, int value);
+void dict_couple_change_value(dict_couple couple, int value[]);
 void dict_couple_free(dict_couple *couple);
