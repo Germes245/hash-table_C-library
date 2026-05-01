29dd53efc9716bd7dcbec61583c04301bce7ff96
shya <dan119@bk.ru>
2026-05-01 21:35:18 +0300

    test of hash_table_chain passed

--- src/hash_table_chain.c
+++ src/hash_table_chain.c
@@ -24,8 +24,10 @@ static void str_hash_table_chain_shift_left(str_hash_table_chain *chain, size_t 
     }
 }
 
-static size_t str_hash_table_chain_has_couple_with_key(str_hash_table_chain *chain, char* key){ //если есть, то индекс структуры, иначе 0
+size_t str_hash_table_chain_has_couple_with_key(str_hash_table_chain *chain, char* key){ //если есть, то индекс структуры, иначе 0
+    //printf("длина %d\n", chain->length);
     for(size_t i = 0; i < chain->length; i++){
+        //printf("%d %s %s\n", i, chain->array[i].key,key);
         if(strcmp(chain->array[i].key,key) == 0) return i+1;
     }
     return 0;
@@ -39,20 +41,20 @@ str_hash_table_chain str_hash_table_chain_init(){
 }
 
 void str_hash_table_chain_put(str_hash_table_chain *chain, char* key, char* value){
-    if(chain->length){
-        int index;
-        if((index = str_hash_table_chain_has_couple_with_key(chain, key)) != -1){
+    if(chain->length){ // если в цепочке что-то есть
+        size_t index = str_hash_table_chain_has_couple_with_key(chain, key);
+        if(index != 0){ // если в цепочке есть пара, где есть ключ, который равен в новой паре
             free(chain->array[index].value);
             create_and_copy(chain->array[index].value, value);
+            chain->length++;
         }
-        else{
+        else{ // если в цепочке нет пара, где есть ключ, который равен в новой паре
             chain->array = realloc(chain->array, (++chain->length)*sizeof(str_hash_table_chain));
             create_and_copy(chain->array[chain->length-1].key, key);
             create_and_copy(chain->array[chain->length-1].value, value);
         }
-        chain->length++;
     }
-    else{
+    else{ // если в цепочке ничего нет
         chain->array = malloc(sizeof(str_hash_table_couple));
         chain->length = 1;
         create_and_copy(chain->array[0].key, key);
@@ -73,7 +75,7 @@ char* str_hash_table_chain_get(str_hash_table_chain chain, char* key){
 unsigned char str_hash_table_chain_delete(str_hash_table_chain *chain, char* key){
     size_t index = str_hash_table_chain_has_couple_with_key(chain, key);
     //printf("%d\n", index);
-    assert(index==0);
+    //assert(index==0);
     if(index == 0) return 0;
     //printf("%s\n", chain->array[index-1].key);
     str_hash_table_couple_free(&chain->array[index-1]);
@@ -81,8 +83,10 @@ unsigned char str_hash_table_chain_delete(str_hash_table_chain *chain, char* key
         str_hash_table_chain_shift_left(chain, index-1);
         chain->array = realloc(chain->array, (--chain->length)*sizeof(str_hash_table_couple));
     }
-    free(chain->array);
-    chain->array=0;
+    else{
+        free(chain->array);
+        chain->array=0;
+    }
     return 1;
 }
 
--- tests/test_chain.c
+++ tests/test_chain.c
@@ -3,14 +3,6 @@
 #include <assert.h>
 #include <string.h>
 
-static size_t str_hash_table_chain_has_couple_with_key(str_hash_table_chain *chain, char* key){ //если есть, то индекс структуры, иначе 0
-    for(size_t i = 0; i < chain->length; i++){
-        printf("string: %s, and %s\n", chain->array[i].key, key);
-        if(strcmp(chain->array[i].key,key) == 0) return i+1;
-    }
-    return 0;
-}
-
 int main(){
     str_hash_table_chain chain = str_hash_table_chain_init();
 
@@ -18,15 +10,23 @@ int main(){
     assert(chain.length == 0);
 
     str_hash_table_chain_put(&chain, "key1", "value1");
-    assert(chain.length == 1);
+    assert(chain.length == 1 && strcmp(chain.array[0].key, "key1") == 0);
 
     str_hash_table_chain_put(&chain, "key2", "value2");
-    assert(chain.length == 2);
+    assert(chain.length == 2 && strcmp(chain.array[1].key, "key2") == 0);
 
-    char* val = str_hash_table_chain_get(chain, "key2");
+    //size_t index = str_hash_table_chain_has_couple_with_key(&chain, "key2");
+    char* val = str_hash_table_chain_get(chain, "key1");
+    //printf("%d\n", index);
     assert(val != 0);
 
-    printf("%d\n", str_hash_table_chain_has_couple_with_key(&chain, "key1"));
+    //printf("%d\n", str_hash_table_chain_has_couple_with_key(&chain, "key1"));
     str_hash_table_chain_delete(&chain, "key1");
+
+    val = str_hash_table_chain_get(chain, "key1");
+
+    assert(val == 0);
+
+    str_hash_table_chain_free(&chain);
     return 0;
 }
