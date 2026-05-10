661dbdbceef1a8d6336fbd0172756f86de3e05cd
shya <dan119@bk.ru>
2026-05-10 20:03:41 +0300

    исправление критической ошибки в str_hash_table_chain_put

--- tests/test.c
+++ tests/test.c
@@ -2,6 +2,7 @@
 #include <dyn_array.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,27 +12,50 @@ str_hash_table_chain str_hash_table_chain_init(){
     return dyn_array_init(0);
 }
 
-void str_hash_table_chain_put(str_hash_table_chain *chain, char* key, char* value){
-    size_t length_for_string = strlen(key)+1;
-    char* array = malloc(length_for_string);
-    memcpy(array, key, length_for_string);
-    dyn_array_append(chain, array);
+static size_t find_index_of_key(str_hash_table_chain chain, char* key, uint8_t *has_string){
+    for(size_t i = 0; i < chain.length; i+=2){
+        if(strcmp(key, chain.data[i]) == 0) {
+            *has_string = 1;
+            return i;
+        }
+    }
+    *has_string = 0;
+}
 
-    length_for_string = strlen(value)+1;
-    array = malloc(length_for_string);
-    memcpy(array, value, length_for_string);
-    dyn_array_append(chain, array);
+void str_hash_table_chain_put(str_hash_table_chain *chain, char* key, char* value){
+    uint8_t has_string;
+    size_t index = find_index_of_key(*chain, key, &has_string);
+    if (has_string) {
+        size_t length_for_string = strlen(value)+1;
+        array->data[index+1] = realloc(array->data[index+1], length_for_string);
+        memcpy(array->data[index+1], value, length_for_string);
+    }
+    else{
+        size_t length_for_string = strlen(key)+1;
+        char* array = malloc(length_for_string);
+        memcpy(array, key, length_for_string);
+        dyn_array_append(chain, array);
+
+        length_for_string = strlen(value)+1;
+        array = malloc(length_for_string);
+        memcpy(array, value, length_for_string);
+        dyn_array_append(chain, array);
+    }
 }
 
 char* str_hash_table_chain_get_pointer(str_hash_table_chain chain, char* key){
-    for(size_t i = 0; i < chain.length; i+=2){
-        if(strcmp(key, chain.data[i]) == 0) return chain.data[i+1];
+    uint8_t has_string;
+    size_t index = find_index_of_key(chain, key, &has_string);
+    if (has_string) {
+        return chain.data + index;
     }
+    return 0;
 }
 
 int main(){
     str_hash_table_chain chain = str_hash_table_chain_init();
     str_hash_table_chain_put(&chain, "shya", "sha");
+    str_hash_table_chain_put(&chain, "shya", "shaka");
     printf("%s\n", str_hash_table_chain_get_pointer(chain, "shya"));
     return 0;
 }
