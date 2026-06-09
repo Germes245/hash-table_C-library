6929e627b47268770fd8c3573760703c5d138ede
shya <dan119@bk.ru>
2026-06-09 22:47:31 +0300

    вроде рабочий код

--- tests/test.c
+++ tests/test.c
@@ -8,23 +8,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef dyn_array_size_t hash_table;
+typedef dyn_array_size_t hash_table_chain;
 
-hash_table hash_table_init(size_t length){
+hash_table_chain hash_table_chain_init(size_t length){
     length*=2;
-    hash_table array = dyn_array_size_t_init(length);
+    hash_table_chain array = dyn_array_size_t_init(length);
     for(size_t i = 0; i < length; i++){
         array.data[i]=0;
     }
     return array;    
 }
 
-void hash_table_put(hash_table *chain, size_t key, size_t value){
+void hash_table_chain_put(hash_table_chain *chain, size_t key, size_t value){
     dyn_array_size_t_append(chain, key);
     dyn_array_size_t_append(chain, value);
 }
 
-size_t hash_table_get_number(hash_table chain, uint8_t *error, size_t key){
+size_t hash_table_chain_get_number(hash_table_chain chain, uint8_t *error, size_t key){
     for(size_t i = 0; i < chain.length; i+=2){
         if(chain.data[i] == key){
             *error = 0;
@@ -32,13 +32,15 @@ size_t hash_table_get_number(hash_table chain, uint8_t *error, size_t key){
         }
     }
     *error = 1;
+    return 0;
 }
 
 int main(){
-    hash_table chain = hash_table_init(5);
-    hash_table_put(&chain, 2, 6);
-    //hash_table_put(&chain, 2, 10);
-    //hash_table_delete(chain, "shya");
-    //printf("%d\n", hash_table_get_pointer(chain, "shya"));
+    hash_table_chain chain = hash_table_chain_init(5);
+    hash_table_chain_put(&chain, 2, 6);
+    //hash_table_chain_put(&chain, 2, 10);
+    //hash_table_chain_delete(chain, "shya");
+    uint8_t error;
+    printf("%d\n", hash_table_chain_get_number(chain, &error, 2));
     return 0;
 }
