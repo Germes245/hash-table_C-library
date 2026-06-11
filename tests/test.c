427e7ef26c91e2e4693dd9f39774bae8a95cf84c
shya <dan119@bk.ru>
2026-06-11 21:25:16 +0300

    добавлен метод delete для цепочек

--- tests/test.c
+++ tests/test.c
@@ -1,7 +1,5 @@
-//void
-//#include <dyn_array_int.h>
+
 #include <dyn_array_size_t.h>
-//#include <dyn_string.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -25,6 +23,13 @@ void hash_table_chain_put(hash_table_chain *chain, size_t key, size_t value){
     dyn_array_size_t_append(chain, value);
 }
 
+/*static size_t find_index_of_key(hash_table_chain chain, uint8_t *error, size_t key){
+    for(size_t i = 0; i < chain.length; i+=2){
+        if(chain.data[i] == key){
+            *error = 0;
+            return i;
+}*/
+
 size_t hash_table_chain_get_number(hash_table_chain chain, uint8_t *error, size_t key){
     for(size_t i = 0; i < chain.length; i+=2){
         if(chain.data[i] == key){
@@ -36,8 +41,18 @@ size_t hash_table_chain_get_number(hash_table_chain chain, uint8_t *error, size_
     return 0;
 }
 
-size_t hash_table_chain_delete(hash_table_chain chain, size_t key);
-сутра сердца
+size_t hash_table_chain_delete(hash_table_chain chain, uint8_t *error, size_t key){
+    for(size_t i = 0; i < chain.length; i+=2){
+        if(chain.data[i] == key){
+            *error = 0;
+            dyn_array_size_t_delete(chain, i, error);
+            return dyn_array_size_t_delete(chain, i, error);
+        }
+    }
+    *error = 1;
+    return 0;
+}
+//сутра сердца
 
 int main(){
     hash_table_chain chain = hash_table_chain_init(5);
@@ -45,5 +60,6 @@ int main(){
     uint8_t error;
     size_t number = hash_table_chain_get_number(chain, &error, 2);
     assert(error == 0 && number == 6);
+    number = hash_table_chain_delete
     return 0;
 }
